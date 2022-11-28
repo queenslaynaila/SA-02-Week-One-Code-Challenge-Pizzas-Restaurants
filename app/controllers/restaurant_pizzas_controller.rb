a7ea0c81e2d07801c548637d5af4cfa458e3d75d
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-11-28 11:27:29 +0300

    debug validation rescue

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,8 +1,7 @@
 class RestaurantPizzasController < ApplicationController
-  rescue_from ActiveRecord::RecordInvalid, with: :render_not_found_response
-
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   def create
-    restaurant_pizza = RestaurantPizza.create(pizza_params)
+    restaurant_pizza = RestaurantPizza.create!(pizza_params)
     render json:restaurant_pizza.pizza, status: :created
   end
 
@@ -10,8 +9,8 @@ class RestaurantPizzasController < ApplicationController
   def pizza_params
     params.permit(:pizza_id, :restaurant_id, :price)
   end
-
   def render_unprocessable_entity_response(invalid)
-    render json: {errors:"validation errors"}, status: :unprocessable_entity
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
   end
+
 end
