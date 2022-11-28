b304c10f64a9deead225569d99b7bfeecbaa1a82
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-11-28 11:14:00 +0300

    debug rescue

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantPizzasController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_not_found_response
 
   def create
     restaurant_pizza = RestaurantPizza.create(pizza_params)
